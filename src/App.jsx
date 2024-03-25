ba5003fab163331e26d2a7b4468b2d26cceef9ca
Regina Jalandoni <reginajalandoni13@gmail.com>
2024-03-25 15:25:39 +0800

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p className='text-6xl text-yellow-800'>hello</p>
+      </div>
+    </>
+  )
+}
+
+export default App
