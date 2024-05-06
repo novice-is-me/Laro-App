6d7489825755a591e35bc643d5426f34df51ee0f
Alwyn <122137166+Aweng204@users.noreply.github.com>
2024-05-06 15:47:46 +0800

    Revised Org Dashboard

--- src/pages/OrganizerDashContent.jsx
+++ src/pages/OrganizerDashContent.jsx
@@ -1,101 +1,152 @@
-// OrganizerDashContent.js
-import React, { useState } from 'react';
-import Calendar from 'react-calendar';
+import React from 'react';
+import Calendar from "react-calendar";
 
-const OrganizerDashContent = () => {
-    const [date, setDate] = useState(new Date());
-    const [showOptions, setShowOptions] = useState(false);
+const OrganizerDashContent = ({ activeTab }) => {
+    const weeks = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
 
-    const toggleOptions = () => {
-        setShowOptions(!showOptions);
-    }
+    const times = Array.from({ length: 17 }, (_, index) => {
+        const hour = index % 12 || 12; // Convert 0 to 12
+        const ampm = index < 12 ? 'AM' : 'PM';
+        return `${hour} ${ampm}`;
+    });
 
     return (
-        <div className='flex flex-col px-4 py-2 w-full lg:w-[1000px] ' style={{ height:  '100vh'}}>
-            {/* Create Button */}
-            <div className='flex flex-col justify-end   w-full'>
-                    {/* Third & Fourth columns */}
-                    <div className='flex justify-between  pl-2 gap-4 w-full h-[90%]'>
-                        {/* Third column */}
-                        <div className='flex flex-col gap-5 w-full'>
-                        <div className='bg-black rounded-[10px] flex flex-col justify-between px-4 py-2 gap-4 h-[35%] '>
-                            <div>
-        <p className='font-Poppins font-semibold text-[30px] text-white'>
-            Credits
-        </p>
-        <p className='font-Poppins text-gray text-[15px] pb-1 pl-2'>
-            PHP
-        </p>
-        <p className='font-Poppins font-semibold text-white text-[30px]'>
-            7,000
-        </p>
-    </div>
-    <div className='w-[50%]  mb-4 lg:w-full '>
-        <p className='font-Poppins text-white text-[14px]'>
-            You need to reach 10,000 before checkout.
-        </p>
-    </div>
-</div>
-
-<div className='flex flex-col bg-orange text-white rounded-[10px] gap-2'>
-      <div className='justify-between items-start pt-4 pl-4 pr-4 '>
-        <button className="flex justify-between  w-[100%]">
-          <p className='font-Inter font-semibold text-[30px]'>Calendar</p>
-          <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 512" className="pt-2">
-            <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" fill='white'/>
-          </svg>
-        </button>
-      </div>
-      <div className="flex justify-center items-center pb-4">
-        <Calendar
-          onChange={setDate}
-          value={date}
-        />
-      </div>
-    </div>
-
-
-
-
-            </div>
-                        {/* Fourth column */}
-                        <div className='flex flex-col gap-6 h-full w-full'>
-
-                        <div className='bg-orange text-white rounded-[10px] px-4 py-2 gap-4 h-[35%]'>
-                        
-                        <p className='font-Inter font-semibold text-[30px]'>
-                            Earning History</p>
-                            <p className='font-Poppins font-semibold text-[30px] pt-10'>
-                            0</p>
-                        
+        <div className="content">
+            {activeTab === 'Overview' && (
+                <div className='flex flex-col justify-center items-center w-full h-screen'>
+                    {/* Upper Boxes */}
+                    <div className='flex justify-center items-end gap-5 w-full h-fit px-10 py-2.5'>
+                        {/* First Box */}
+                        <div className='flex flex-col justify-between px-10 py-5 w-1/2 h-[265px] rounded-[10px] text-white font-Inter bg-black'>
+                            <div className='flex flex-col w-full h-full'>
+                                <p className='text-[30px] font-semibold'>Credits</p>
+                                <p className='text-gray'>PHP</p>
+                            </div>
+                            <div className='flex items-center w-full h-full'>
+                                <p className='text-[40px] font-semibold'>7,000</p>
+                            </div>
+                            <div className='flex items-end w-full h-full'>
+                                <p className='text-[15px] font-light'>You need to reach 10,000 before checkout.</p>
+                            </div>   
                         </div>
 
-                        <div className='bg-black text-white rounded-[10px] flex justify-between px-4 py-2 gap-4 h-[65%]'>
-                        <p className='font-Inter font-semibold text-[30px]'>
-                                Match History</p>
-
+                        {/* Second Box */}
+                        <div className='flex flex-col justify-between px-10 py-5 w-1/2 h-[265px] rounded-[10px] text-white font-Inter bg-orange'>
+                            <div className='flex flex-col w-full h-1/3 '>
+                                <p className='text-[30px] font-semibold'>Earnings History</p>
+                            </div>
+                            <div className='flex items-start w-full h-2/3 '>
+                                <p className='text-[40px] font-semibold'>0</p>
+                            </div>
                         </div>
-                        </div>
-                        
                     </div>
-                   
-            {/* Create New Event Button */}
-            <div className='flex justify-center gap-4 border border-orange pt-4'>
-                {showOptions && (
-                    <button className="bg-orange text-white font-Poppins p-4 rounded-[30px]" onClick={toggleOptions}>
-                        x Create New Event
-                    </button>
-                )}
-                {!showOptions && (
-                    <button className="bg-orange text-white font-Poppins p-4 rounded-[30px]" onClick={toggleOptions}>
-                        + Create New Event
-                    </button>
-                )}
-                </div>
-                </div>
-            </div>
+                    
+                    {/* Lower Boxes */}
+                    <div className='flex justify-center items-center gap-5 w-full h-fit px-10 py-2.5'>
+                        {/* Third Box */}
+                        <div className='flex flex-col justify-between px-10 py-5 w-1/2 h-[537px] rounded-[10px] gap-5 text-white font-Inter bg-orange'>
+                            <div className='flex justify-between items-center w-full h-fit '>
+                                <p className='text-[30px] font-semibold'>Calendar</p>
+                                <p className='px-3 text-[40px] font-bold'>&#62;</p>
+                            </div>
 
+                            <div className='flex justify-center text-black w-full h-full'>
+                                <Calendar className='w-full h-fit font-Poppins' formatShortWeekday={(locale, date) => weeks[date.getDay()]}/>
+                            </div>
+                        </div>
 
+                        {/* Fourth Box */}
+                        <div className='flex flex-col justify-between px-10 py-5 w-1/2 h-[537px] rounded-[10px] text-white font-Inter bg-black'>
+                            <div className='flex flex-col w-full h-full'>
+                                <p className='text-[30px] font-semibold'>Match History</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    
+                    
+                </div>
+            )}
+            {activeTab === 'Activities' && (
+                <div className='flex flex-col items-center w-full h-screen px-5 gap-5'>
+                    {/* Header */}
+                    <div className='flex justify-between items-center p-5 w-full h-fit font-Inter'>
+                        <p className='text-[40px] font-semibold'>My Activity</p>
+                        <div className='flex items-center gap-5'>
+                            <p>Image</p>
+                            <p className='text-[25px] font-semibold'>History</p>
+                        </div>
+                    </div>
+
+                    {/* Buttons */}
+                    <div className='flex justify-between w-full p-5 text-[25px] font-Inter shadow-lg'>
+                        <button className='w-[185px] h-[65px] rounded-[200px] text-white font-bold bg-orange'>This week</button>
+                        <button className='w-[185px] h-[65px]'>This Month</button>
+                        <button className='w-[185px] h-[65px]'>Later</button>
+                    </div>
+
+                    {/* History Cards */}
+                    <div className='w-full h-full flex flex-col py-5 gap-5 overflow-y-auto'>
+                        {/* Individual Card */}
+                        <div className='flex flex-col px-10 py-5 gap-5 w-full h-fit rounded-[20px] font-Inter border border-[#B0B0B0] shadow-lg'>
+                            <div className='flex justify-between'>
+                                <p className='text-[30px] text-orange font-semibold'>Game Event No. 1</p>
+                                <p>Image Arrow Button</p>
+                            </div>
+                            <p className='text-[25px] text-[#525252]'>Saturday, March 16, 2024. Mandaluyong Court</p>
+                        </div>
+
+                        <div className='flex flex-col px-10 py-5 gap-5 w-full h-fit rounded-[20px] font-Inter border border-[#B0B0B0] shadow-lg'>
+                            <div className='flex justify-between'>
+                                <p className='text-[30px] text-orange font-semibold'>Game Event No. 2</p>
+                                <p>Image Arrow Button</p>
+                            </div>
+                            <p className='text-[25px] text-[#525252]'>Saturday, March 16, 2024. Mandaluyong Court</p>
+                        </div>
+
+                        <div className='flex flex-col px-10 py-5 gap-5 w-full h-fit rounded-[20px] font-Inter border border-[#B0B0B0] shadow-lg'>
+                            <div className='flex justify-between'>
+                                <p className='text-[30px] text-orange font-semibold'>Game Event No. 3</p>
+                                <p>Image Arrow Button</p>
+                            </div>
+                            <p className='text-[25px] text-[#525252]'>Saturday, March 16, 2024. Mandaluyong Court</p>
+                        </div>
+
+                        <div className='flex flex-col px-10 py-5 gap-5 w-full h-fit rounded-[20px] font-Inter border border-[#B0B0B0] shadow-lg'>
+                            <div className='flex justify-between'>
+                                <p className='text-[30px] text-orange font-semibold'>Game Event No. 4</p>
+                                <p>Image Arrow Button</p>
+                            </div>
+                            <p className='text-[25px] text-[#525252]'>Saturday, March 16, 2024. Mandaluyong Court</p>
+                        </div>
+
+                        <div className='flex flex-col px-10 py-5 gap-5 w-full h-fit rounded-[20px] font-Inter border border-[#B0B0B0] shadow-lg'>
+                            <div className='flex justify-between'>
+                                <p className='text-[30px] text-orange font-semibold'>Game Event No. 5</p>
+                                <p>Image Arrow Button</p>
+                            </div>
+                            <p className='text-[25px] text-[#525252]'>Saturday, March 16, 2024. Mandaluyong Court</p>
+                        </div>
+
+                        <div className='flex flex-col px-10 py-5 gap-5 w-full h-fit rounded-[20px] font-Inter border border-[#B0B0B0] shadow-lg'>
+                            <div className='flex justify-between'>
+                                <p className='text-[30px] text-orange font-semibold'>Game Event No. 6</p>
+                                <p>Image Arrow Button</p>
+                            </div>
+                            <p className='text-[25px] text-[#525252]'>Saturday, March 16, 2024. Mandaluyong Court</p>
+                        </div>
+                    </div>
+
+                    {/* Create New Event Button */}
+                    <div className='flex justify-center items-center w-full h-fit font-Inter p-5'>
+                        <div className='flex justify-evenly items-center w-[295px] h-[70px] text-white text-[20px] font-semibold bg-orange rounded-[50px]'>
+                            <p className='flex items-center h-full text-[40px]'>+</p>
+                            <p className='flex items-center h-full'>Create New Event</p>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
     );
 }
 
